7869ef9c2a72e25f7c91c1a9dd810c5174b8c3db
TseYeong <xieyun220823@gmail.com>
2024-12-29 10:14:43 +0800

    Common list tools method

--- src/list_tools.py
+++ src/list_tools.py
@@ -1,6 +1,6 @@
 class Tools:
     @staticmethod
-    def find_max_value(lst: list):
+    def find_max_value(lst: list) -> tuple[int, int]:
         """
         Find the max value and its index in list.
         :param lst: A list.
@@ -14,4 +14,28 @@ class Tools:
         max_value = max(lst)
         index = lst.index(max_value)
         return index, max_value
+
+    @staticmethod
+    def normalization(lsts: list) -> list:
+        """
+        Normalize the data to the [0,1] interval.
+        :param lsts: List of lists containing data to be normalized.
+        :type lsts: list
+        :return: Normalized data.
+        :rtype: list
+        """
+        flat_list = [x for lst in lsts for row in lst for x in row]
+
+        min_value = min(flat_list)
+        max_value = max(flat_list)
+
+        normalized = [
+            [
+                [
+                    (x - min_value) / (max_value - min_value) for x in row
+                ] for row in lst
+            ] for lst in lsts
+        ]
+
+        return normalized
 
